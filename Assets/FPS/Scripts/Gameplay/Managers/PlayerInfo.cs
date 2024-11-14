5394c36723e15323e17f7fe41ecd0ba9fd50ad4b
Sage Phillips <sage242@hotmail.com>
2024-11-13 22:30:06 -0600

    Added PlayerInfo.cs so I can store the points the player has earned there.

---
+++ Assets/FPS/Scripts/Gameplay/Managers/PlayerInfo.cs
@@ -0,0 +1,21 @@
+﻿using Unity.FPS.Game;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Unity.FPS.Gameplay
+{
+    public class PlayerInfo : MonoBehaviour
+    {
+        void Awake()
+        {
+        }
+
+        void Start()
+        {
+        }
+
+        void Update()
+        {
+        }
+    }
+}
